e51861fbcc62192011c2353463dcb53defc6e2b6
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-08-08 18:23:08 +0000

    added the admin classes

--- proj_movie/collection/admin.py
+++ proj_movie/collection/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
-
+from django.contrib import messages
+from django.utils.translation import ngettext
+from .models import Collection
 # Register your models here.
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+
+    @admin.action(description="Mark as favourite")
+    def mark_favourite(self, request, queryset):
+        queryset.update(is_favourite=True)
+        self.message_user(
+            request,
+            ngettext(
+                "%d collection was successfully marked as favourite.",
+                "%d collections were successfully marked as favourite.",
+                queryset.count(),
+            )
+            % queryset.count(),
+            messages.SUCCESS,
+        )
+
+    @admin.action(description="Remove from favourite")
+    def unmark_favourite(self, request, queryset):
+        queryset.update(is_favourite=False)
+        self.message_user(
+            request,
+            ngettext(
+                "%d collection was successfully unmarked as favourite.",
+                "%d collections were successfully unmarked as favourite.",
+                queryset.count(),
+            )
+            % queryset.count(),
+            messages.SUCCESS,
+        )
+
+    list_display = ["id",
+                    "user",
+                    "collection_title",
+                    "is_favourite",
+                    "created_on",
+                    "updated_on"]
+    search_fields = ["id",
+                     "collection_title",
+                     "user__id",
+                     "user__email",
+                     "user__username",
+                     "is_favourite"]
+    list_filter = ["is_favourite"]
+    actions = [mark_favourite, unmark_favourite]
+
--- proj_movie/movies/admin.py
+++ proj_movie/movies/admin.py
@@ -1,3 +1,67 @@
 from django.contrib import admin
-
+from django.contrib import messages
+from django.utils.translation import ngettext
+from .models import CollectionMovies, Genres
 # Register your models here.
+
+
+@admin.register(CollectionMovies)
+class CollectionMovieAdmin(admin.ModelAdmin):
+
+    @admin.action(description="Mark as favourite")
+    def mark_favourite(self, request, queryset):
+        queryset.update(is_favourite=True)
+        self.message_user(
+            request,
+            ngettext(
+                "%d collection was successfully marked as favourite.",
+                "%d collections were successfully marked as favourite.",
+                queryset.count(),
+            )
+            % queryset.count(),
+            messages.SUCCESS,
+        )
+
+    @admin.action(description="Remove from favourite")
+    def unmark_favourite(self, request, queryset):
+        queryset.update(is_favourite=False)
+        self.message_user(
+            request,
+            ngettext(
+                "%d collection was successfully unmarked as favourite.",
+                "%d collections were successfully unmarked as favourite.",
+                queryset.count(),
+            )
+            % queryset.count(),
+            messages.SUCCESS,
+        )
+
+    list_display = ["id",
+                    "collection",
+                    "movie_title",
+                    "is_favourite",
+                    "created_on",
+                    "updated_on"]
+    search_fields = ["id",
+                     "movie_title",
+                     "genres",
+                     "is_favourite",
+                     "collection__id",
+                     "collection__collection_title"]
+    list_filter = ["is_favourite"]
+    actions = [mark_favourite, unmark_favourite]
+
+
+@admin.register(Genres)
+class GenresAdmin(admin.ModelAdmin):
+    list_display = ["id",
+                    "user",
+                    "genre",
+                    "movie_count",
+                    "created_on",
+                    "updated_on"]
+    search_fields = ["id",
+                     "user__id",
+                     "user__email",
+                     "user__username",
+                     "genre"]
