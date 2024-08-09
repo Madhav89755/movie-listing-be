33dbc189da5fb33c3f2747561f8d4c130ac17af5
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-08-09 06:30:54 +0000

    fetch updated

--- proj_movie/movies/fetch.py
+++ proj_movie/movies/fetch.py
@@ -2,32 +2,31 @@ import requests
 import base64
 from django.conf import settings
 from utils.logging import unknown_exception_logger
-from utils.static_messages import (API_RETRY_MAX_LIMIT_REACHED, 
+from utils.static_messages import (API_RETRY_MAX_LIMIT_REACHED,
                                    API_REQUEST_FAILED)
 
 credentials = f'{settings.API_USERNAME}:{settings.API_PASSWORD}'
-encoded_credentials = base64.b64encode(credentials.encode('utf-8')).decode('utf-8')
+encoded_credentials = base64.b64encode(
+    credentials.encode('utf-8')).decode('utf-8')
 
 headers = {
     'Authorization': f'Basic {encoded_credentials}',
-    "User-Agent":"Thunder Client (https://www.thunderclient.com)",
-    "Accept":"*/*"
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Accept": "*/*"
 }
 
-def fetch_movies(page:int=None, retry_limit:int=3)->tuple[bool|int, str|dict]:
-    if retry_limit==0:
-        print(API_RETRY_MAX_LIMIT_REACHED)
-        print("======================================================")
-        return False, API_RETRY_MAX_LIMIT_REACHED
-    try:
-        if page:
-            page=f'page={page}'
-        response = requests.get(f"{settings.API_URL}?{page or ''}", headers=headers, verify=False)
-        if response.status_code != 200:
-            raise Exception(API_REQUEST_FAILED.format(response.status_code))
-        return response.status_code, response.json()
-    except Exception as e:
-        print(f"Exception occurred for retry number {retry_limit}")
-        unknown_exception_logger(e)
-        retry_limit-=1
-        fetch_movies(page, retry_limit)
+
+def fetch_movies(page: int = None, retry_limit: int = 3) -> tuple[bool | int, str | dict]:
+    for x in range(0,retry_limit):
+        try:
+            print(f"Movie API Hit #{x+1}")
+            url=settings.API_URL
+            if page:
+                url = f'{url}?page={page}'
+            resp = requests.get(
+                f"{url}", headers=headers, verify=False)
+            response=(resp.status_code, resp.json())
+        except Exception as e:
+            unknown_exception_logger(e)
+            response=(404, API_RETRY_MAX_LIMIT_REACHED)
+    return response
